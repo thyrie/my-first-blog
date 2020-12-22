3999b2f82927eb50b81020aacbc8853a16dd2a4a
Jack Piet Thyrie <thyrie@gmail.com>
2020-12-22 15:39:42 +0100

    My Django Girls app, first commit

---
+++ djangoGirls.py
@@ -0,0 +1,11 @@
+def hi(name1, name2):
+    print('Hi there! ' + name1)
+    print('How are you? ' + name2)
+
+hi("jack", "Ellouise")
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name, name)
+    print('Next girl')
+
